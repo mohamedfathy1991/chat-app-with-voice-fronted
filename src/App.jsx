3dbe278c5dea3d48c2aa5273921445271a18a5ad
mohamedfathy1991 <mohamed.electronics1991@gmail.com>
2025-08-26 16:46:58 +0300

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,30 @@
- import{ createBrowserRouter, createHashRouter, RouterProvider} from "react-router-dom"
-  
-import './App.css'
-import Layout from './components/Layout/Layout'
-import Chat from "./components/pages/Chat"
-import Home from "./components/pages/Home"
-import Chatprovider from "./context/chatContext"
- 
+import { createHashRouter, RouterProvider } from "react-router-dom";
+
+import './App.css';
+import Layout from './components/Layout/Layout';
+import Chat from "./components/pages/Chat";
+import Home from "./components/pages/Home";
+import Chatprovider from "./context/chatContext";
+
 function App() {
-  const routes= createHashRouter([
-     {path: "",
+  const routes = createHashRouter([
+    {
+      path: "/",            // path الأساسي
       element: <Layout />,
-      children:[{
-        path:"/",
-        element: <Home/> },
-        {
-          path:"/chat",
-          element: <Chat/> 
-        }
-    
-    
-    ]
+      children: [
+        { path: "/", element: <Home /> },
+        { path: "/chat", element: <Chat /> }
+      ]
     }
-  ])
- 
+  ]);
 
   return (
-    <>
-     
-     <div className="app">
-           <Chatprovider>
-        <RouterProvider router={routes} >
-              <Layout/>
-
-          </RouterProvider>
-           </Chatprovider>
-        </div>
-      
-
-    </>
-     
-  )
+    <Chatprovider>
+      <div className="app">
+        <RouterProvider router={routes} />
+      </div>
+    </Chatprovider>
+  );
 }
 
-export default App
+export default App;
